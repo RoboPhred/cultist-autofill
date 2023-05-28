b28b477038312e076dde36371e8843de550cfe62
RoboPhred <robophred@gmail.com>
2023-05-28 11:01:35 -0700

    Update for steam workshop

--- CultistAutofill.cs
+++ CultistAutofill.cs
@@ -4,114 +4,153 @@ using System.Linq;
 using SecretHistories.Entities;
 using SecretHistories.Enums;
 using SecretHistories.Spheres;
+using SecretHistories.Fucine;
 using SecretHistories.UI;
+using SecretHistories.Services;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace CultistAutofill
+public class CultistAutofill : MonoBehaviour, ISettingSubscriber
 {
-    [BepInEx.BepInPlugin("net.robophreddev.CultistSimulator.CultistAutofill", "CultistAutofill", "1.0.1")]
-    public class CultistAutofillMod : BepInEx.BaseUnityPlugin
+    private static readonly string KeySetting = "autofill_trigger";
+
+    private Key binding;
+
+    void Start()
     {
-        void Start()
+        NoonUtility.LogWarning($"Hello World! {this.GetType().Name}");
+        var setting = Watchman.Get<Compendium>().GetEntityById<Setting>(CultistAutofill.KeySetting);
+        setting.AddSubscriber(this);
+    }
+
+    void OnDestroy()
+    {
+        var setting = Watchman.Get<Compendium>().GetEntityById<Setting>(CultistAutofill.KeySetting);
+        setting.RemoveSubscriber(this);
+    }
+
+    public static void Initialise()
+    {
+        try
         {
-            this.Logger.LogInfo("CultistAutofill initialized.");
+            var component = new GameObject().AddComponent<CultistAutofill>();
+        }
+        catch (Exception e)
+        {
+            NoonUtility.LogException(e);
+        }
+    }
+
+    void Update()
+    {
+        if (Keyboard.current[this.binding].wasPressedThisFrame)
+        {
+            this.AutofillSituation();
+        }
+    }
+
+    void ReadBinding()
+    {
+        var setting = Watchman.Get<Compendium>().GetEntityById<Setting>(CultistAutofill.KeySetting).CurrentValue as string;
+        this.binding = (Key)Enum.Parse(typeof(Key), setting);
+    }
+
+    void ISettingSubscriber.BeforeSettingUpdated(object newValue)
+    {
+
+    }
+
+    void ISettingSubscriber.WhenSettingUpdated(object newValue)
+    {
+        var strValue = newValue as string;
+        Watchman.Get<Config>().PersistConfigValue(CultistAutofill.KeySetting, strValue);
+        this.binding = (Key)Enum.Parse(typeof(Key), strValue);
+    }
+
+    void AutofillSituation()
+    {
+        if (Watchman.Get<Numa>().IsOtherworldActive())
+        {
+            return;
         }
 
-        void Update()
+        var situation = this.GetOpenSituation();
+        if (situation == null)
         {
-            if (Keyboard.current.rKey.wasPressedThisFrame)
-            {
-                this.AutofillSituation();
-            }
+            return;
         }
 
-        void AutofillSituation()
+        var candidates = this.GetTokensOrderedByDistance(situation.GetRectTransform().anchoredPosition).ToArray();
+
+        this.TryFillSpheres(() => situation.GetSpheresActiveForCurrentState(), candidates);
+    }
+
+    void TryFillSpheres(Func<IList<Sphere>> slotsResolver, IList<Token> candidates)
+    {
+        // We handle the first slot independently, as chooing a first slot will
+        //  usually determine what other slots are available.
+        var primarySlot = slotsResolver().FirstOrDefault();
+        if (primarySlot == null)
         {
-            // if (TabletopManager.IsInMansus())
-            // {
-            //     return;
-            // }
-
-            var situation = this.GetOpenSituation();
-            if (situation == null)
-            {
-                return;
-            }
-
-            var candidates = this.GetTokensOrderedByDistance(situation.GetRectTransform().anchoredPosition).ToArray();
-
-            var activeSpheres = situation.GetSpheresActiveForCurrentState();
-            this.TryFillSpheres(() => activeSpheres, candidates);
+            return;
         }
 
-        void TryFillSpheres(Func<IList<Sphere>> slotsResolver, IList<Token> candidates)
+        if (!this.TrySatisfySphere(primarySlot, candidates))
         {
-            // We handle the first slot independently, as chooing a first slot will
-            //  usually determine what other slots are available.
-            var primarySlot = slotsResolver().FirstOrDefault();
-            if (primarySlot == null)
-            {
-                return;
-            }
-
-            if (!this.TrySatisfySphere(primarySlot, candidates))
-            {
-                return;
-            }
-
-            // Fill the remaining slots.
-            //  We need to re-fetch the slots, as slotting the primary may
-            //  provide us with new slots.
-            var slots = slotsResolver();
-            foreach (var slot in slots.Skip(1))
-            {
-                this.TrySatisfySphere(slot, candidates);
-            }
+            return;
         }
 
-        bool TrySatisfySphere(Sphere sphere, IEnumerable<Token> candidates)
+        // Fill the remaining slots.
+        //  We need to re-fetch the slots, as slotting the primary may
+        //  provide us with new slots.
+        var slots = slotsResolver();
+        foreach (var slot in slots.Skip(1))
         {
-            if (sphere.GetElementStacks().Count > 0)
+            this.TrySatisfySphere(slot, candidates);
+        }
+    }
+
+    bool TrySatisfySphere(Sphere sphere, IEnumerable<Token> candidates)
+    {
+        if (sphere.GetElementStacks().Count > 0)
+        {
+            // Already something in the sphere.
+            return true;
+        }
+
+        foreach (var token in candidates)
+        {
+            // TryAcceptToken handles UI concerns like messages and sound effects, so we need to check manually
+            // to avoid spamming sound effects.
+            if (sphere.GetMatchForTokenPayload(token.Payload).MatchType == SlotMatchForAspectsType.Okay)
             {
-                // Already something in the sphere.
+                // We sitll need to TryAcceptToken, as that contains the logic to split the stack into one card.
+                sphere.TryAcceptToken(token, new Context(Context.ActionSource.PlayerDrag));
                 return true;
             }
-
-            foreach (var token in candidates)
-            {
-                // TryAcceptToken handles UI concerns like messages and sound effects, so we need to check manually
-                // to avoid spamming sound effects.
-                if (sphere.GetMatchForTokenPayload(token.Payload).MatchType == SlotMatchForAspectsType.Okay)
-                {
-                    // We sitll need to TryAcceptToken, as that contains the logic to split the stack into one card.
-                    sphere.TryAcceptToken(token, new Context(Context.ActionSource.PlayerDrag));
-                    return true;
-                }
-            }
-
-            return false;
         }
 
-        IEnumerable<Token> GetTokensOrderedByDistance(Vector2 fromPoint)
-        {
-            return from sphere in Watchman.Get<HornedAxe>().GetSpheres()
-                   where sphere.SphereCategory == SphereCategory.World
-                   from stack in sphere.GetElementStacks()
-                   orderby CalcDistance(stack.GetRectTransform().anchoredPosition, fromPoint)
-                   select stack.Token;
-        }
+        return false;
+    }
 
-        float CalcDistance(Vector2 a, Vector2 b)
-        {
-            var xDist = a.x - b.x;
-            var yDist = a.y - b.y;
-            return (float)Math.Sqrt((xDist * xDist) + (yDist * yDist));
-        }
+    IEnumerable<Token> GetTokensOrderedByDistance(Vector2 fromPoint)
+    {
+        return from sphere in Watchman.Get<HornedAxe>().GetSpheres()
+               where sphere.SphereCategory == SphereCategory.World
+               from stack in sphere.GetElementStacks()
+               orderby CalcDistance(stack.GetRectTransform().anchoredPosition, fromPoint)
+               select stack.Token;
+    }
 
-        Situation GetOpenSituation()
-        {
-            return Watchman.Get<HornedAxe>().GetRegisteredSituations().Find(x => x.IsOpen);
-        }
+    float CalcDistance(Vector2 a, Vector2 b)
+    {
+        var xDist = a.x - b.x;
+        var yDist = a.y - b.y;
+        return (float)Math.Sqrt((xDist * xDist) + (yDist * yDist));
+    }
+
+    Situation GetOpenSituation()
+    {
+        return Watchman.Get<HornedAxe>().GetRegisteredSituations().Find(x => x.IsOpen);
     }
 }
